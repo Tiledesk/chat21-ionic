a9e9e0376c394320baaf36b2a3f166a609fcedd9
Gabriele Panico <gabriele.panico@gmail.com>
2025-10-14 08:59:33 +0200

    minor

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -6,7 +6,7 @@ export function getOSCode(key: string, token: string): boolean {
       const keys: String[] = token.split("-");
 
       let element = keys.find(el => el.includes(key))
-      console.log('keys', keys)
+      // console.log('keys', keys)
       if(element){
         element = element.split(":")[1]
         if(element && element === "F"){
