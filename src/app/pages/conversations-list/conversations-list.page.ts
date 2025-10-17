59987e721094648d7fd364256ba3fd1e6469b50b
Dario De Pascalis <dariodepa75@gmail.com>
2025-10-17 17:16:12 +0200

    Bug fixed: Scrolling to the last message when opening a conversation Bug fixed: Loading in the conversation list disabled when removing the last conversation

--- src/app/pages/conversations-list/conversations-list.page.ts
+++ src/app/pages/conversations-list/conversations-list.page.ts
@@ -581,7 +581,10 @@ export class ConversationListPage implements OnInit {
     })
 
     this.conversationsHandlerService.conversationRemoved.subscribe((conversation: ConversationModel) => {
-        this.logger.log('[CONVS-LIST-PAGE] ***** conversationsRemoved *****',conversation)
+        this.logger.log('[CONVS-LIST-PAGE] ***** conversationsRemoved *****',conversation);
+        if (this.conversations.length === 0) {
+          this.loadingIsActive = false;
+        }
     })
 
     this.archivedConversationsHandlerService.archivedConversationAdded.subscribe((conversation: ConversationModel) => {
